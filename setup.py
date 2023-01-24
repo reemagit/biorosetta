e432ef2a71420994cde44d473c561ab4b6160d84
Enrico Maiorino <enrico.maiorino@gmail.com>
2023-01-24 14:34:30 -0500

    readme changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from pathlib import Path
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README_pypi.md").read_text()
 
 setup(
 	name='biorosetta',
